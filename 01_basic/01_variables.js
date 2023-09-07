d2d55403082fc025f92d179aaa838a5eb7ac4d04
Satyajeet singh Gusain <139236116+Satyajeet8218@users.noreply.github.com>
2023-09-07 06:51:18 +0000

    Variables/Datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 7456088
+let accountEmail = "satyajeet@gmail.com"
+var accountPassword="456789"
+accountcity ="Pauri Garhwal"
+
+let accountState;
+
+
+//accountId = 2// it is not allowed 
+
+accountEmail ="jeetu@gmail.com"
+accountPassword="00000"
+accountcity="Kotdwara"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
+
+/* prefer not use to var because of issues in block scope and functional scope */
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  // It will treat all Js code as a newer version //
+
+//alert( 3+3 )  we are using node js not browser in node js there is differt fuction to use alert//
+
+console.log(3+3)  // Code readablity should be good 
+console.log("Satyajeet singh gusain")
+
+
+
+//There are some data types 
+
+//string datatype it is using between the double code or single code""
+//number type datatypes there number limit it can be 52 
+//Boolean type datatypes  (false,true) 
+//bigint datatype is use full for if number limit is to big 
+//null datatypes = standlone value
+//undefined datatypes = when we are not give any value
+//symbol = whenever we will take unique values
+                            
+//These all are primative datatypes
+
+
+console.log(typeof "satyajeet singh gusain")  // If we will use typeof in the console.log it will show output what datatypes we written
+
+
