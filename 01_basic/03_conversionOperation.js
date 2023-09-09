e77d57f9f39f33e7e86696f48c85fa967d7ab477
Satyajeet singh Gusain <139236116+Satyajeet8218@users.noreply.github.com>
2023-09-09 12:49:21 +0000

    Add comparision

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -33,3 +33,52 @@ let stringNumber=String(someNumber)
 
 console.log(stringNumber);
 console.log (typeof stringNumber);
+
+
+    //................ operation.......................//
+
+
+    let value=3
+    let negValue= -value
+    console.log(negValue);
+
+
+/*
+
+    console.log(2+2);
+    cosole.log(2-2);
+    cosole.log(3*3);
+    console.log(3**3);
+    console.log(3/3);
+    console.log(3%3);
+
+*/
+
+/*
+let str1="satyajeet"
+let str2="Gusain"
+let str3= str1 + str2
+
+console.log(str3)*/
+
+/*
+
+console.log("1"+2);
+
+console.log(2+"2");
+
+console.log("1"+2+2);
+
+console.log(1+1+"2");
+
+*/
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = (100)
+++gameCounter;
+console.log(gameCounter);
+
+
+
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,27 @@
+/*
+ console.log(2>1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log (2 != 1);
+*/
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+
+
+//That type of cnversion make us confuse 
+
+
+console.log(null > 0);
+console.log(null == 0);      //In null sintex Comparision convert null to a number, treating as a zero.thats why (3) null >=0 is true and(1)null>0is false.
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log (undefined >= 0);
+console.log (undefined < 0);
+
+
+//  (===) triple Equal alwys compare values and datatypes both.
