52313b237c2c90711a101b2e8f048e2913b41f32
Satyajeet singh Gusain <139236116+Satyajeet8218@users.noreply.github.com>
2023-09-15 14:14:17 +0000

    Added Object part 2

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,51 @@
+//sigleton objects
+//const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name ="satya"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+
+const regularUser = {
+
+   email: "some@gmail.com",
+   fullname: {
+
+    userfullname:{
+
+        firstname: "satyajeet",
+        lastname: "gusain",
+
+    }
+}
+
+}
+
+//console.log(regularUser.fullname.userfullname.lastname);
+
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+//const obj3 = {obj1, obj2}
+
+//const obj3 = Object.assign({}, obj1, obj2 ,obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}  // usefull or easy sintex.
+//console.log(obj3);
+
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(Object.hasOwnProperty('isLoggedOut'));
