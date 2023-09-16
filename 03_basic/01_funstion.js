22fb5bd78f6510ae9aba8fb79cef82b494c72757
Satyajeet singh Gusain <139236116+Satyajeet8218@users.noreply.github.com>
2023-09-16 13:23:35 +0000

    Functions

---
+++ 03_basic/01_funstion.js
@@ -0,0 +1,75 @@
+/*
+function sayMyName(){
+
+    console.log("afsdb");
+    console.log("fsdb");
+    console.log("aphh");
+    console.log("sdgjmb");
+    console.log("ijh4h54");
+    console.log("7uu");
+    console.log("ronalod");
+    console.log("akjfhjkhgkjg");
+    
+}
+*/
+
+//sayMyName()  // sayMyName is refrence of fuction or ()peranthisisis the Exicute of function.
+
+
+//In this  function  number1 or number2 is perimeters.
+
+function addTwoNumbers(number1, number2){
+
+      console.log(number1 + number2);
+}
+
+//addTwoNumbers(10, 10)  // Under the bracet numbers (10, 10) are called as Arguments when give call.
+
+//addTwoNumbers(10, "10")
+
+const result = addTwoNumbers(10, 10)
+//console.log("result:", result);
+
+    //method for print result
+
+   // function addTwoNumbers(number1, number2){
+    //let result = number1 + number2
+    //return result
+    //console,log("satyajeet singh gusaim");  // after declare result we can't print other value.
+//}
+
+
+//function addTwoNumbers(number1, number2){
+
+    //let result = number1 + number2
+    //console.log("satyajeet singh gusain")  // That time it will print because it is upto the result.
+   // return result
+//}
+
+ //on more shortcut for get result.
+
+ function addTwoNumbers(number1,number2){
+
+    return number1 + number2
+
+ }
+
+   // How we can take values.
+
+ function  loginUserMessage(username = "sonu"){
+    if(username === undefined){                        // (!) usefull it will convert true to false and vice varsa.
+        console.log("please enter a username");
+        return
+    }
+
+  return `${username} justlogged in`
+
+ }
+
+// console.log(loginUserMessage("satyajeet singh gusain"));
+
+  console.log(loginUserMessage("satyajeet singh gusain"));    // If we will not give values then it will print as undefined.
+
+
+  
+
